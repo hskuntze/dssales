9bf186c2d60658c266116478d212945cccb8fc81
Hassan <hskuntze@gmail.com>
2022-06-02 18:13:01 -0300

    Filter Component

---
+++ front-web/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Header = () => {
+  return (
+    <header className="main-header-container">
+      <h1 className="header-title">DS Sales</h1>
+    </header>
+  );
+};
+
+export default Header;
